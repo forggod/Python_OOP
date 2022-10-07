2c8bb05ff9518659fba35ec4121c45b4344c8e92
PC1 <mrpri@PONCHEC-PC>
2022-10-07 23:23:32 +0300

    Created

---
+++ main.py
@@ -0,0 +1,26 @@
+# TODO: Задание 5. Разработать класс «Домашняя библиотека».
+#  Предусмотреть возможность работы с произвольным числом книг, поиска книг по автору,
+#  названию, году издания, добавления книг в библиотеку, удаления и
+#  сортировки по разным признакам.
+
+
+class HomeLibrary:
+    library = []
+
+    def __init__(self, name, author, year):
+        self.name = name
+        self.author = author
+        self.year = year
+        HomeLibrary.library.append((self.name, self.author, self.year))
+
+    @staticmethod
+    def show():
+        for i in HomeLibrary.library:
+            print(*i)
+
+
+book1 = HomeLibrary('Онегин', 'Пушкин', '1833')
+book2 = HomeLibrary('Мцыри', 'Лермонтов', '1840')
+book4 = HomeLibrary('Война и мир', 'Толстой', '1865')
+
+HomeLibrary.show()
