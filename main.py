b806f0c1cddbc9dcfa33ebeeb59c4363ac33f528
PC1 <mrpri@PONCHEC-PC>
2022-10-08 09:22:18 +0300

    Added author search

--- main.py
+++ main.py
@@ -5,22 +5,34 @@
 
 
 class HomeLibrary:
-    library = []
+    __library = []
 
     def __init__(self, name, author, year):
         self.name = name
         self.author = author
         self.year = year
-        HomeLibrary.library.append((self.name, self.author, self.year))
+        HomeLibrary.__library.append((self.name, self.author, self.year))
 
     @staticmethod
     def show():
-        for i in HomeLibrary.library:
+        for i in HomeLibrary.__library:
             print(*i)
+
+    @staticmethod
+    def to_string(line):
+
+        return f'{line[0]} {line[1]} {line[2]}'
+
+    @staticmethod
+    def search_author(author):
+        for i in HomeLibrary.__library:
+            if author == i[1]:
+                return HomeLibrary.to_string(i)
 
 
 HomeLibrary('Онегин', 'Пушкин', '1833')
 HomeLibrary('Мцыри', 'Лермонтов', '1840')
 HomeLibrary('Война и мир', 'Толстой', '1865')
 
+print(HomeLibrary.search_author('Пушкин'))
 HomeLibrary.show()
